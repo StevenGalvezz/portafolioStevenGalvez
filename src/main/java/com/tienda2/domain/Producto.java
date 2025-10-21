d62b59ed9b6e41b9f1d300c6793c3926977da5bb
xsf <xsf@DESKTOP-5GJCLST>
2025-10-21 01:04:55 -0600

    semana 6 incompleta

--- src/main/java/com/tienda2/domain/Producto.java
+++ src/main/java/com/tienda2/domain/Producto.java
@@ -17,8 +17,8 @@ public class Producto implements Serializable {
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     @Column(name = "id_producto")
     private Integer idProducto;
-    private Integer idCategoria;
-
+    //private Integer idCategoria; ya no se usa por la anotación @ManyToOne *semana 6*
+    
     @Column(nullable = false, length = 50)
     @NotBlank(message = "La descripción no puede estar vacía.")
     @Size(max = 50, message = "La descripción no puede tener más de 50 caracteres.")
@@ -39,4 +39,9 @@ public class Producto implements Serializable {
     @Column(name = "ruta_imagen", length = 1024)
     private String rutaImagen;
     private boolean activo;
+    
+    @ManyToOne
+    @JoinColumn(name = "id_categoria")
+    private Categoria categoria;
+
 }
