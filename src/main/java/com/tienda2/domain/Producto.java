61020636cade1d82462047f6f55edcd672b4754b
xsf <xsf@DESKTOP-5GJCLST>
2025-11-29 18:54:36 -0600

    Semana 11

--- src/main/java/com/tienda2/domain/Producto.java
+++ src/main/java/com/tienda2/domain/Producto.java
@@ -17,7 +17,7 @@ public class Producto implements Serializable {
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     @Column(name = "id_producto")
     private Integer idProducto;
-    // private Integer idCategoria; ya no se usa por la anotación @ManyToOne
+    //private Integer idCategoria; //ya no se usa por la anotación @ManyToOne
 
     @Column(nullable = false, length = 50)
     @NotBlank(message = "La descripción no puede estar vacía.")
